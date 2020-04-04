57f236422f5d14e4b2bc926f85773072c14af4db
GavrikGal <Gavrik.gal@gmail.com>
2020-04-04 09:34:15 +0300

    Домашка 3, проходят все тесты

--- homeworks/text_history/text_history.py
+++ homeworks/text_history/text_history.py
@@ -15,58 +15,54 @@ class TextHistory:
         return self._text_history[-1].version
 
     def action(self, action):
+        if action.pos is None:
+            action.pos = len(self.text)
         pos = action.pos
         text = action.text
         from_version = action.from_version
         to_version = action.to_version
         length = action.length
+
+        if self.version < from_version:
+            raise ValueError()
+        elif pos > len(self.text) or pos < 0:
+            raise ValueError()
+
         if isinstance(action, InsertAction):
-            if pos is None:
-                new_text = self.text + text
-            else:
-                if pos > len(self.text) or pos < 0:
-                    raise ValueError()
-                new_text = self.text[:pos] + text + self.text[pos:]
-            self._text_history.append(Text(new_text, to_version, action))
+            text = self.text[:pos] + text + self.text[pos:]
 
         if isinstance(action, ReplaceAction):
-            if pos is None:
-                new_text = self.text + text
-            else:
-                if pos > len(self.text) or pos < 0:
-                    raise ValueError()
-                new_text = self.text[:pos] + text + self.text[pos+len(text):]
-            self._text_history.append(Text(new_text, to_version, action))
+            text = self.text[:pos] + text + self.text[pos+len(text):]
 
         if isinstance(action, DeleteAction):
-            if pos is None or pos > len(self.text) or pos < 0 or pos + length > len(self.text):
+            if pos is None or pos + length > len(self.text):
                 raise ValueError()
-            else:
-                new_text = self.text[:pos] + self.text[pos+length:]
-            self._text_history.append(Text(new_text, to_version, action))
+            text = self.text[:pos] + self.text[pos+length:]
+
+        self._text_history.append(Text(text, to_version, action))
         return self.version
 
     def get_actions(self, from_version=1, to_version=None):
         if to_version is None:
             to_version = self.version
+        elif to_version > self.version:
+            raise ValueError()
+        elif from_version > to_version:
+            raise ValueError()
+        if from_version < 0:
+            raise ValueError()
         return [text.action for text in self._text_history if from_version < text.version <= to_version]
 
     def insert(self, text, pos=None):
-        cur_version = self.version
-        action = InsertAction(pos=pos, text=text, from_version=cur_version, to_version=cur_version + 1)
-        self.action(action)
+        self.action(InsertAction(pos=pos, text=text, from_version=self.version, to_version=self.version + 1))
         return self.version
 
     def replace(self, text, pos=None):
-        cur_version = self.version
-        action = ReplaceAction(pos=pos, text=text, from_version=cur_version, to_version=cur_version + 1)
-        self.action(action)
+        self.action(ReplaceAction(pos=pos, text=text, from_version=self.version, to_version=self.version + 1))
         return self.version
 
     def delete(self, pos, length):
-        cur_version = self.version
-        action = DeleteAction(pos=pos, length=length, from_version=cur_version, to_version=cur_version + 1)
-        self.action(action)
+        self.action(DeleteAction(pos=pos, length=length, from_version=self.version, to_version=self.version + 1))
         return self.version
 
 
