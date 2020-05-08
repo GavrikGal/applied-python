0e63dd401180ea1f441b7923e644fdfee08c78cc
GavrikGal <Gavrik.gal@gmail.com>
2020-05-08 10:24:32 +0300

    Домашка 5.2. Реализованы все методы стандартного словаря

--- homeworks/dir_dict/dir_dict.py
+++ homeworks/dir_dict/dir_dict.py
@@ -4,45 +4,93 @@ from typing import Iterator
 import os
 
 
-class DirDict(MutableMapping):
-
-    def _update(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
+def _update_dict(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        self._temp_dic = {}
+        try:
             for file_name in os.listdir(self._path):
                 with open(self._path + os.path.sep + file_name) as f:
-                    self._dic[file_name] = f.readline()
-            return func(self, *args, **kwargs)
+                    self._temp_dict[file_name] = f.readline()
+        except OSError:
+            print("can't open " + str(self._path))
+        return func(self, *args, **kwargs)
+    return wrapper
 
-        return wrapper
 
-    def __delitem__(self, v: '_KT') -> None:
-        pass
+class DirDict(MutableMapping):
+    def __init__(self, path):
+        self._path = path
+        self._temp_dict = {}
+        if not os.path.isdir(self._path):
+            os.mkdir(path)
 
-    @_update
+    @_update_dict
+    def __delitem__(self, k: '_KT') -> None:
+        if k in self:
+            try:
+                os.remove(self._path + os.path.sep + k)
+            except FileNotFoundError:
+                print('item with key: ' + str(self._path + os.path.sep + k) + ' not found')
+            except PermissionError as error:
+                print("can't delete item with key: " + str(self._path + os.path.sep + k) + str(error.args[0]))
+
+    @_update_dict
     def __getitem__(self, k: '_KT') -> '_VT_co':
-        return self._dic[k]
+        return self._temp_dict[k]
 
     def __len__(self) -> int:
         return len(os.listdir(self._path))
 
-    @_update
+    @_update_dict
     def __iter__(self) -> Iterator['_T_co']:
-        for i in self._dic.items():
+        for i in self._temp_dict.items():
             yield i
 
+    @_update_dict
+    def items(self):
+        for i in self:
+            yield i
+
+    @_update_dict
+    def keys(self):
+        for k in self._temp_dict.keys():
+            yield k
+
+    @_update_dict
+    def values(self):
+        for v in self._temp_dict.values():
+            yield v
+
     def __setitem__(self, k: '_KT', v: '_VT') -> None:
-        pass
+        try:
+            with open(self._path + os.path.sep + k, 'w') as f:
+                f.write(v)
+                f.close()
+        except OSError:
+            print("can't write data on disk")
 
-    def __init__(self, path):
-        self._path = path
-        self._dic = {}
-        if not os.path.isdir(self._path):
-            os.mkdir(path)
 
-
-d = DirDict('d:\Temp\dir_dict')
-print('len() - ', len(d))
+test_d = DirDict('d:\Temp\dir_dict')
+print('len() - ', len(test_d))
 print('items:')
-for k, v in d:
-    print('\t' + str(k) + ' - ' + str(v))
+for key, value in test_d:
+    print('\t' + str(key) + ' - ' + str(value))
+test_d['test_write'] = 'string to test to write'
+print('len() - ', len(test_d))
+print('update items:')
+for key, value in test_d:
+    print('\t' + str(key) + ' - ' + str(value))
+del test_d['test_write']
+print('len() - ', len(test_d))
+print('update items after deleting:')
+for key, value in test_d.items():
+    print('\t' + str(key) + ' - ' + str(value))
+
+print('test keys:')
+for key in test_d.keys():
+    print(key, end=', ')
+
+print('test value:')
+for value in test_d.values():
+    print(value, end=', ')
