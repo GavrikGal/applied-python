ebe99e016c0990056ab0bd732b7146b1684bf846
GavrikGal <Gavrik.gal@gmail.com>
2020-05-13 16:25:08 +0300

    Домашка 6. Реализовано добавление пользователей. Начал организовывавать нормальную архитектуру проекта

--- homeworks/blog/app.py
+++ homeworks/blog/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request, url_for
 from homeworks.blog.DBcm import UseDataBase, ConnectionError, CredentialsError, SQLError
 
 
@@ -14,10 +14,10 @@ app.config['dbconfig'] = {'host': '127.0.0.1',
 @app.route('/')
 def entry_page() -> 'html':
     return render_template('index.html',
-                           the_title='Welcome to blog', )
+                           the_title='Привет! Это какой-то блог', )
 
 
-@app.route('/users')
+@app.route('/users', methods=['GET'])
 def users_page() -> 'html':
 
     try:
@@ -39,6 +39,32 @@ def users_page() -> 'html':
         print('Is your query correct? Error:', str(err))
     except Exception as err:
         print('Something went wrong:', str(err))
+    return 'Error'
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user() -> 'html':
+
+    try:
+        with UseDataBase(app.config['dbconfig']) as cursor:
+            _SQL = """INSERT INTO users 
+                    (first_name, last_name, login, password) 
+                    VALUES 
+                    (%s, %s, %s, %s)"""
+            cursor.execute(_SQL, (request.form['first_name'],
+                                  request.form['last_name'],
+                                  request.form['login'],
+                                  request.form['password'],))
+            return redirect('users')
+    except ConnectionError as err:
+            print('Something went wrong:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:', str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+
     return 'Error'
 
 
